97b3bc50912d73ff477dfd8bbc7c09b276e98d0b
kikuke <kikuke@kikukeui-MacBookAir.local>
2024-10-20 16:41:38 +0900

    [ADD] Implement Write Command

--- Main_Study/ASW/Module/SSD1306/SSD1306.c
+++ Main_Study/ASW/Module/SSD1306/SSD1306.c
@@ -46,7 +46,75 @@ static void SetVerticalScrollArea(uint8 fixedRows, uint8 scrollRows);
 /*  Addressing Setting Command  */
 /********************************/
 
+/**
+ * @brief Set the lower nibble of the column start address register for Page Mode
+ * 
+ * @param nibble lower nibble
+ */
+static void SetLowColStartAddrPageMode(uint8 nibble);
 
+/**
+ * @brief Set the higher nibble of the column start address register for Page Addressing Mode
+ * 
+ * @param nibble higher nibble
+ */
+static void SetHighColStartAddrPageMode(uint8 nibble);
+static void SetMemoryAddressingMode(uint8 mode);
+static void SetComlumnAddress(uint8 startAddr, uint8 endAddr);
+static void SetPageAddress(uint8 startAddr, uint8 endAddr);
+/**
+ * @brief Set GDDRAM Page Start Address(PAGE0 ~ PAGE7) for Page Addressing Mode
+ * 
+ * @param page 
+ */
+static void SetPageStartForPageMode(uint8 page);
+
+/*************************************/
+/*  Hardware Configuration Command   */
+/* Panel resolution & layout related */
+/*************************************/
+
+static void SetDisplayStartLine(uint8 line);
+static void SetSegmentReMap(uint8 b127);
+/**
+ * @brief Set MUX ratio to N+1 MUX
+ * 
+ * @param mux 
+ */
+static void SetMultiplexRatio(uint8 mux);
+static void SetComOutputScanDirection(uint8 bRemap);
+static void SetDisplayOffset(uint8 com);
+static void SetComPinsHardwareConfig(uint8 config);
+
+/*******************************************/
+/* Timing & Driving Scheme Setting Command */
+/*******************************************/
+
+/**
+ * @brief Set Display Clock Divide Ratio / Oscilator Frequency
+ * 
+ * @param divRatio 
+ * @param oscFreq 
+ */
+static void SetDisplayClockRatioFreq(uint8 divRatio, uint8 oscFreq);
+static void SetPreChargePeriod(uint8 phase1, uint8 phase2);
+static void VcomhDeselectLevel(uint8 level);
+static void NOP_CMD(void);
+
+/*******************************************/
+/*              Read Command               */
+/*******************************************/
+
+/**
+ * @brief return display On Off
+ * 
+ * @return uint8 1 for display OFF / 0 for display ON
+ */
+static uint8 StatusRegisterRead(void); // TODO: Need To Implement
+
+/*******************************************/
+/*       I2C Communication Function        */
+/*******************************************/
 static void _SendData(SSD1306_Packet_T type, const uint8 *data, Ifx_SizeT size);
 static void _SendPacket(SSD1306_Packet_T type, const uint8 *packet, Ifx_SizeT size);
 static void _MakePacket(uint8 *buffer, SSD1306_Packet_T type, const uint8 *data, Ifx_SizeT size);
@@ -151,6 +219,152 @@ static void SetVerticalScrollArea(uint8 fixedRows, uint8 scrollRows)
         };
 
     _SendData(SSD1306_Packet_COMMAND, cmd, 3);
+}
+
+static void SetLowColStartAddrPageMode(uint8 nibble)
+{
+    uint8 cmd[1] = {(0x00 | (0x0F & nibble))};
+
+    _SendData(SSD1306_Packet_COMMAND, cmd, 1);
+}
+
+static void SetHighColStartAddrPageMode(uint8 nibble)
+{
+    uint8 cmd[1] = {(0x10 | (0x0F & nibble))};
+
+    _SendData(SSD1306_Packet_COMMAND, cmd, 1);
+}
+
+static void SetMemoryAddressingMode(uint8 mode)
+{
+    uint8 cmd[2] = {
+            (0x20),
+            (0x03 & mode)
+        };
+
+    _SendData(SSD1306_Packet_COMMAND, cmd, 2);
+}
+
+static void SetComlumnAddress(uint8 startAddr, uint8 endAddr)
+{
+    uint8 cmd[3] = {
+            (0x21),
+            (0x7F & startAddr),
+            (0x7F & endAddr)
+        };
+
+    _SendData(SSD1306_Packet_COMMAND, cmd, 3);
+}
+
+static void SetPageAddress(uint8 startAddr, uint8 endAddr)
+{
+    uint8 cmd[3] = {
+            (0x22),
+            (0x07 & startAddr),
+            (0x07 & endAddr)
+        };
+
+    _SendData(SSD1306_Packet_COMMAND, cmd, 3);
+}
+
+static void SetPageStartForPageMode(uint8 page)
+{
+    uint8 cmd[1] = {(0xB0 | (0x07 & page))};
+
+    _SendData(SSD1306_Packet_COMMAND, cmd, 1);
+}
+
+static void SetDisplayStartLine(uint8 line)
+{
+    uint8 cmd[1] = {(0x40 | (0x3F & line))};
+
+    _SendData(SSD1306_Packet_COMMAND, cmd, 1);
+}
+
+static void SetSegmentReMap(uint8 b127)
+{
+    uint8 cmd[1] = {(0xA0 | (0x01 & b127))};
+
+    _SendData(SSD1306_Packet_COMMAND, cmd, 1);
+}
+
+static void SetMultiplexRatio(uint8 mux)
+{
+    uint8 cmd[2] = {
+            (0xA8),
+            (0x3F & mux)
+        };
+
+    _SendData(SSD1306_Packet_COMMAND, cmd, 2);
+}
+
+static void SetComOutputScanDirection(uint8 bRemap)
+{
+    uint8 cmd[1] = {(0xC0 | (0x08 & (bRemap << 3)))};
+
+    _SendData(SSD1306_Packet_COMMAND, cmd, 1);
+}
+
+static void SetDisplayOffset(uint8 com)
+{
+    uint8 cmd[2] = {
+            (0xD3),
+            (0x3F & com)
+        };
+
+    _SendData(SSD1306_Packet_COMMAND, cmd, 2);
+}
+
+static void SetComPinsHardwareConfig(uint8 config)
+{
+    uint8 cmd[2] = {
+            (0xDA),
+            ((0x30 & (config << 4)) | (0x02))
+        };
+
+    _SendData(SSD1306_Packet_COMMAND, cmd, 2);
+}
+
+static void SetDisplayClockRatioFreq(uint8 divRatio, uint8 oscFreq)
+{
+    uint8 cmd[2] = {
+            (0xD5),
+            ((0xF0 & (oscFreq << 4)) | (0x0F & divRatio))
+        };
+
+    _SendData(SSD1306_Packet_COMMAND, cmd, 2);
+}
+
+static void SetPreChargePeriod(uint8 phase1, uint8 phase2)
+{
+    uint8 cmd[2] = {
+            (0xD9),
+            ((0xF0 & (phase2 << 4)) | (0x0F & phase1))
+        };
+
+    _SendData(SSD1306_Packet_COMMAND, cmd, 2);
+}
+
+static void VcomhDeselectLevel(uint8 level)
+{
+    uint8 cmd[2] = {
+            (0xDB),
+            (0x70 & (level << 4))
+        };
+
+    _SendData(SSD1306_Packet_COMMAND, cmd, 2);
+}
+
+static void NOP_CMD(void)
+{
+    uint8 cmd[1] = {0xE3};
+
+    _SendData(SSD1306_Packet_COMMAND, cmd, 1);
+}
+
+static uint8 StatusRegisterRead(void)
+{
+    return 0;
 }
 
 static void _SendData(SSD1306_Packet_T type, const uint8 *data, Ifx_SizeT size)
