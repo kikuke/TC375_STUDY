268ecff6563b96fb9703ad611c75784f73913f92
kikuke <kkkjs0814@naver.com>
2024-10-26 18:09:44 +0900

    [ADD] Set Pixel

--- Main_Study/ASW/Module/SSD1306/SSD1306.h
+++ Main_Study/ASW/Module/SSD1306/SSD1306.h
@@ -8,14 +8,23 @@
  * Reference : [SSD1780 DataSheet](https://cdn-shop.adafruit.com/datasheets/SSD1306.pdf)
  */
 
-#define SSD1306_I2C_BAUDRATE            1000000                 /* Table 13-6 : tIdle = 1.3us
-                                                                   Maximum Baudrate = 1.3 * 8(bps) * 1000*1000(us to s) = 1040000 */
+#define SSD1306_I2C_BAUDRATE            1000000                                         /* Table 13-6 : tIdle = 1.3us
+                                                                                          Maximum Baudrate = 1.3 * 8(bps) * 1000*1000(us to s) = 1040000 */
 #define SSD1306_SEG_LEN                 8
 #define SSD1306_MAX_SEG                 128
 #define SSD1306_MAX_PAGE                8
 #define SSD1306_I2C_BUFF_MAX            (SSD1306_MAX_SEG * 2)
 
-#define GET_SSD1306_ADDR(bSA0) (uint8)( (0x3C) | (0x01 & bSA0) )
+#if 0
+#define SSD1306_MAX_SEND_DATA           8
+#else
+#define SSD1306_MAX_SEND_DATA           2
+#endif
+#define SSD1306_MAX_SEND_SIZE           SSD1306_MAX_SEND_DATA * 2                       /* Max Send Data: 16Byte (Control 8Byte, Data 8Byte) */
+
+#define SSD1306_ADDR_128_32             0
+#define SSD1306_ADDR_128_64             1
+#define GET_SSD1306_ADDR(bSA0) (uint8)( (0x3C) | (0x01 & bSA0) )                        /* 0x3C for 128 * 32, 0x3D for 128 * 64 */
 
 typedef enum eSSD1306_Packet_T {
     SSD1306_Packet_COMMAND = 0,
